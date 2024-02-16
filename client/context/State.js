3b4fc53571b9bef6e76944c780c0a07468aa593a
Anekant jain <94368450+Anekantjainsagar@users.noreply.github.com>
2024-02-16 15:01:09 +0530

    Added dcrpyt function and context

---
+++ client/context/State.js
@@ -0,0 +1,17 @@
+"use client";
+import CryptoJS from "crypto-js";
+import Context from "./Context";
+
+const State = (props) => {
+  const dcrpyt = (text) => {
+    var bytes = CryptoJS.AES.decrypt(ciphertext, "MINDMATES");
+    var originalText = bytes.toString(CryptoJS.enc.Utf8);
+    return originalText;
+  };
+
+  return (
+    <Context.Provider value={{ dcrpyt }}>{props.children}</Context.Provider>
+  );
+};
+
+export default State;
