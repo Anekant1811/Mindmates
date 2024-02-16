822029e01057254ea9d74966db5c242be1d1d029
Anekant jain <94368450+Anekantjainsagar@users.noreply.github.com>
2024-02-16 22:54:30 +0530

    Google login added

--- frontend/app/url/index.js
+++ frontend/app/url/index.js
@@ -1,2 +1,4 @@
-const BASE_URL = "https://e35f-115-245-99-238.ngrok-free.app/api";
+// const BASE_URL = "https://e35f-115-245-99-238.ngrok-free.app/api";
+const BASE_URL = "http://localhost:5000/api";
+export const URL = "http://localhost:5000";
 export default BASE_URL;
--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ const login = require("./routes/login");
 const mindmate = require("./routes/mindmate");
 const connectToDb = require("./db/conn");
 const Message = require("./db/schema/messageSchema");
+const OAuth2Strategy = require("passport-google-oauth2").Strategy;
+const User = require("./db/schema/loginSchema");
 
 // Must things
 const app = express();
@@ -97,6 +99,84 @@ io.on("connection", (socket) => {
 
   socket.on("disconnect", () => {
     console.log(`Disconnected`);
+  });
+});
+
+passport.use(
+  new OAuth2Strategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET_ID,
+      callbackURL: "/auth/google/callback",
+      scope: ["profile", "email"],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+
+        if (!user) {
+          user = new User({
+            googleId: profile.id,
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            profile: profile.photos[0].value,
+          });
+
+          await user.save();
+        }
+
+        return done(null, user);
+      } catch (error) {
+        return done(error, null);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: `http://localhost:3000`,
+    failureRedirect: `http://localhost:3000`,
+  })
+);
+
+app.get("/login/sucess", async (req, res) => {
+  if (req.user) {
+    const jwtToken = jwt.sign(
+      {
+        user: req.user._id,
+      },
+      process.env.SECRET_KEY,
+      {
+        expiresIn: "1d",
+      }
+    );
+    res.status(200).json({ message: "user Login", user: req.user, jwtToken });
+  } else {
+    res.status(202).json({ message: "Not Authorized" });
+  }
+});
+
+app.get("/logout", (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect(`http://localhost:3000`);
   });
 });
 
