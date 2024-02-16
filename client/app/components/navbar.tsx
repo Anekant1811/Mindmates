3518f96bff55a89a80c47c030e4c9ca2c60fd552
varun-agrawalclg <buntiagrawal0099@gmail.com>
2024-02-16 14:29:54 +0530

    inital web

---
+++ client/app/components/navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navbar() {
+    const navLinks =[
+        id:1, title:
+    ]
+  return (
+    <div>Navbar
+        <h1>hello</h1>
+    </div>
+  )
+}
