e6d86c35819ac031d0e0453ce4eeccce45b42514
Anekant jain <94368450+Anekantjainsagar@users.noreply.github.com>
2024-02-24 12:02:58 +0530

    Updated all urls

--- frontend/app/url/index.js
+++ frontend/app/url/index.js
@@ -1,6 +1,6 @@
 // const BASE_URL = "https://e35f-115-245-99-238.ngrok-free.app/api";
-const BASE_URL = "http://localhost:5000/api";
-export const URL = "http://localhost:5000/";
+const BASE_URL = "https://mindmates.onrender.com/api";
+export const URL = "https://mindmates.onrender.com/";
 export const SOCKET_URL = "http://localhost:8000/";
 export const PYTHON_URL = "http://127.0.0.1:8888";
 export default BASE_URL;
