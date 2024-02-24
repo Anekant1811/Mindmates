0a0a4640b95df863c62df2cf669de77f16879c97
Anekant jain <94368450+Anekantjainsagar@users.noreply.github.com>
2024-02-24 13:58:10 +0530

    Updated cors origin

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ connectToDb();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://mindmates-seven.vercel.app/"],
+    origin: ["https://mindmates-seven.vercel.app", "http://localhost:3000"],
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
